2e1b339e5eaf7fecf9e214ef81a87da0721a1d47
GROUPINFRA\mdashraful.islam <md.ashraful.islam.de@gmail.com>
2024-05-10 07:50:00 +0200

    remove unncessary comment and add some useful comment

--- install_contao.sh
+++ install_contao.sh
@@ -2,12 +2,13 @@ set -e
 
 composer create-project contao/managed-edition . '4.13.*'
 chown -R www-data:www-data /var/www/html/contao
-# Install Contao
-#composer create-project contao/managed-edition /contao '4.13.*' --no-dev --no-interaction --prefer-dist
-# Create the directory if it doesn't exist
+
+# print composer.json file to see configuration
 cat composer.json
+
 mkdir -p app/config
-# Generate parameters.yml file
+
+# Generate parameters.yml file where environment data from docker-compose.yml file will be saved 
 cat << EOF > app/config/parameters.yml
 parameters:
     database_host: ${MYSQL_HOST}
@@ -16,7 +17,9 @@ parameters:
     database_user: ${MYSQL_USER}
     database_password: ${MYSQL_PASSWORD}
 EOF
+
 cat app/config/parameters.yml
+
 # Run Contao installation commands
 composer install 
 php vendor/bin/contao-console contao:install --no-interaction
